34aa4daa5f36a8c6fc79aa505eaa4095ee77cb6e
deepakphpweb <dipakrana2760@gmail.com>
2021-12-22 00:20:36 +0530

    Added : Welcome to User Registration with lambda expression

---
+++ src/main/java/com/blz/registration/UserRegistration.java
@@ -0,0 +1,7 @@
+package com.blz.registration;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration with lambda expression");
+    }
+}
